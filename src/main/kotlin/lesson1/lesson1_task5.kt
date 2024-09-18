2068697058db4f93acf2269fb43c77f31d784ced
Reysl <rasulnasaraliev@gmail.com>
2024-09-18 12:01:21 +0300

    KS-1-5 flight time

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -3,10 +3,13 @@ package org.example.lesson1
 fun main() {
     val totalSeconds: Short = 6480
 
-    val hours = totalSeconds / 3600
-    val remainingSeconds = totalSeconds % 3600
-    val minutes = remainingSeconds / 60
-    val seconds = remainingSeconds % 60
+    val SECONDS_IN_MINUTE = 60
+    val SECONDS_IN_HOUR = 3600
+
+    val hours = totalSeconds / SECONDS_IN_HOUR
+    val remainingSeconds = totalSeconds % SECONDS_IN_HOUR
+    val minutes = remainingSeconds / SECONDS_IN_MINUTE
+    val seconds = remainingSeconds % SECONDS_IN_MINUTE
 
     val time = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
