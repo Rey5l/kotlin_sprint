3e934a554b75ddbdb54b713fe291a2c77020adc8
Reysl <rasulnasaraliev@gmail.com>
2024-09-18 12:35:18 +0300

    KS-2-1 class average

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson2
+
+fun main() {
+    val numberOfStudents = 4
+
+    val student1 = 3f
+    val student2 = 4f
+    val student3 = 3f
+    val student4 = 5f
+
+    val average = (student1 + student2 + student3 + student4) / numberOfStudents
+    println(average)
+
+}
